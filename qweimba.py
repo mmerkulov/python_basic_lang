de7d8516292e2d33e74ec0ea22431f7193c3c352
mmerkulov <Misha.Merkulov@gmail.com>
2020-09-01 22:34:41 +0700

    Create qweimba.py

---
+++ qweimba.py
@@ -0,0 +1,4 @@
+# Простые числа < 1000
+# print(list(filter(None, map(
+#     lambda y: y * reduce(lambda x, y: x * y != 0, map(lambda x, y=y: y % x, range(2, int(pow(y, 0.5) + 1))), 1),
+#     range(2, 1000)))))
